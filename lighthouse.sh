65b0f8bbd32330eaa07513a72f0d907f7d827fe9
Richard Cheney <richeney@microsoft.com>
2019-11-16 16:29:18 +0000

    Simplified for Cloud Shell

--- lighthouse.sh
+++ lighthouse.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
+##########################################################
+# Simple version to work in Cloud Shell
+##########################################################
 
-cd ~/lighthouse
-source ~/.git-prompt.sh
+az account set --subscription 266282c7-e082-4d48-a6df-56230652be28
 
-source ~/demo-magic.sh
-export DEMO_PROMPT='\[\033[01;32m\]\w\[\033[01;33m\]$(__git_ps1 " (%s)") \[\033[01;37m\]\$ '
-export TYPE_SPEED=40
-export NO_WAIT=true
-export PROMPT_TIMEOUT=1
+echo "az account show"
+az account show --output jsonc
 
-pe 'az account set --subscription 266282c7-e082-4d48-a6df-56230652be28'
-pe 'az account show'
-pe 'uri=https://raw.githubusercontent.com/richeney/lighthouse/master/azurecitadel.managedservicedefinition.json'
-export PROMPT_TIMEOUT=0
-pe 'curl -sSL $uri | jq .'
-pe 'az deployment create --name lighthouse --template-uri $uri --location westeurope'
+echo "uri=https://raw.githubusercontent.com/richeney/lighthouse/master/azurecitadel.managedservicedefinition.json"
+uri=https://raw.githubusercontent.com/richeney/lighthouse/master/azurecitadel.managedservicedefinition.json
+
+echo "curl -sSL $uri | jq ."
+curl -sSL $uri | jq .
+
+echo "az deployment create --name lighthouse --template-uri $uri --location westeurope"
+az deployment create --name lighthouse --template-uri $uri --location westeurope
