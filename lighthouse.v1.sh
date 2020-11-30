3194ca495a1d06f9c324b38be4d433f4dd1e5a3c
richeney <richeney@microsoft.com>
2020-11-30 14:19:26 +0000

    Test for lighthouse updates

--- lighthouse.v1.sh
+++ lighthouse.v1.sh
@@ -6,8 +6,8 @@
 echo "az account show"
 az account show --output jsonc
 
-echo "uri=https://raw.githubusercontent.com/richeney/lighthouse/master/azurecitadel.managedservicedefinition.json"
-uri=https://raw.githubusercontent.com/richeney/lighthouse/master/azurecitadel.managedservicedefinition.json
+uri=https://raw.githubusercontent.com/richeney/lighthouse/master/azurecitadel.v1.json
 
+echo "uri=$uri"
 echo "az deployment sub create --name lighthouse --template-uri $uri --location westeurope"
 az deployment sub create --name lighthouse --template-uri $uri --location westeurope
